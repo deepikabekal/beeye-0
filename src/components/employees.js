55af87b38f85bb37b55e5e9e94812aa59b3179f1
Deepika Bekal <deepika.bekal2@gmail.com>
2021-10-01 02:15:34 +0400

    employees data created

---
+++ src/components/employees.js
@@ -0,0 +1,47 @@
+const employees = [
+    {
+      id: 1,
+      imageURL: "../assets/images/ex.png",
+      employeeName: "Gabrielle Chevalier",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 2,
+      imageName: "employee-2.jpg",
+      employeeName: "Marc Chapuis",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 3,
+      imageName: "employee-3.jpg",
+      employeeName: "Giselle Bissonnette",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 4,
+      imageName: "employee-4.jpg",
+      employeeName: "Mathieu Ancel",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 5,
+      imageName: "employee-5.jpg",
+      employeeName: "Marie-Noëlle Auvray",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 6,
+      imageName: "employee-6.jpg",
+      employeeName: "Nicole Gounelle",
+      designation: "Assistant comptable"
+    },
+    {
+      id: 7,
+      imageName: "employee-7.jpg",
+      employeeName: "Natacha Corriveau",
+      designation: "Assistant comptable"
+    }
+  ];
+  
+  export default employees;
+  
