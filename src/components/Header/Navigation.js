f0063a197e2ca20338eb9ce974c3cadf1dd103e6
Deepika Bekal <deepika.bekal2@gmail.com>
2021-10-01 02:07:53 +0400

    navigation component created

---
+++ src/components/Header/Navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Navigation() {
+  let navLinksInfo = [
+    { id: 1, navName: "Clients", navURL: "#clients" },
+    { id: 2, navName: "Missions", navURL: "#missions" },
+    { id: 3, navName: "Tâches", navURL: "#tâches" },
+    { id: 4, navName: "Équipe", navURL: "#equipe" },
+    { id: 5, navName: "Temps", navURL: "#temps" },
+    { id: 6, navName: "Analyse", navURL: "#analyse" }
+  ];
+
+  return (
+    <nav>
+      <ul>
+        {navLinksInfo.map((item) => (
+          <li key={item.id.toString()} className="li-style">
+            <a className="nav-link" href={item.navURL}>
+              {item.navName}{" "}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
