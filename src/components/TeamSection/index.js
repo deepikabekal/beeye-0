756c725058dc975ade128b9e63fe1e6f185777e1
Deepika Bekal <deepika.bekal2@gmail.com>
2021-10-01 13:30:31 +0400

    team section component created - it will render individual components of the whole section

---
+++ src/components/TeamSection/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ResourceListSection from "./ResourceListSection";
+import "./resourcesection.css";
+
+function TeamSection() {
+  return <ResourceListSection />;
+}
+
+export default TeamSection;
