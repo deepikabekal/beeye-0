c873fa905dcb8eef64f3f7805c0ebad01295846b
Deepika Bekal <deepika.bekal2@gmail.com>
2021-10-01 13:37:19 +0400

    modified resource list component to display the elements correctly

---
+++ src/components/ResourceList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import employees from "./employees";
+
+function ResourceList(props) {
+  return (
+    <div className="resource-list-div div-border">
+      <div className="resource-img-div">
+        <img src={props.imageURL} alt="employee of the company" />
+      </div>
+      <div className="resource-info-div">
+        <p className="resource-p-tag">{props.employeeName}</p>
+        <p className="resource-p-tag grayed-text">{props.designation}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ResourceList;
