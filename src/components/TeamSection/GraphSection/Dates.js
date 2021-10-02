c5b909aa0d1735f7fa5a42f8297ca13ed939edac
Deepika Bekal <deepika.bekal2@gmail.com>
2021-10-02 12:04:03 +0400

    created dates data for the graph

---
+++ src/components/TeamSection/GraphSection/Dates.js
@@ -0,0 +1,88 @@
+const dates = [
+    {
+      date: 28,
+      month: "Fév",
+      fill: 70,
+      color: "#B7BAD6"
+    },
+    {
+      date: 1,
+      month: "Mar",
+      fill: 70,
+      color: "#B7BAD6"
+    },
+    {
+      date: 2,
+      month: "Mar",
+      fill: 70,
+      color: "#B7BAD6"
+    },
+    {
+      date: 3,
+      month: "Mar",
+      fill: 70,
+      color: "#B7BAD6"
+    },
+    {
+      date: 4,
+      month: "Mar",
+      fill: 70,
+      color: "#B7BAD6"
+    },
+    {
+      date: 5,
+      month: "Mar",
+      fill: 100,
+      color: "#EF3059"
+    },
+    {
+      date: 6,
+      month: "Mar",
+      fill: 100,
+      color: "#F1F0FA"
+    },
+    {
+      date: 7,
+      month: "Mar",
+      fill: 100,
+      color: "#F1F0FA"
+    },
+    {
+      date: 8,
+      month: "Mar",
+      fill: 50,
+      color: "#34344B"
+    },
+    {
+      date: 9,
+      month: "Mar",
+      fill: 30,
+      color: "#F27633"
+    },
+    {
+      date: 10,
+      month: "Mar",
+      fill: 100,
+      color: "#EF3059"
+    },
+    {
+      date: 11,
+      month: "Mar",
+      fill: 100,
+      color: "#F1F0FA"
+    },
+    {
+      date: 12,
+      month: "Mar",
+      fill: 100,
+      color: "#F1F0FA"
+    },
+    {
+      date: 13,
+      month: "Mar",
+      fill: 100,
+      color: "#F1F0FA"
+    }
+  ];
+  
+  export default dates;
